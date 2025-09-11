ae6fd9f75f0798eb067c8f78a03d28c3c1cc77b7
ArtemAzanov <azanov16@yandex.ru>
2025-09-11 10:57:02 +0300

    C#FirstWork

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("введите x:");
+
+double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("введите y:");
+double y = Convert.ToDouble(Console.ReadLine());
+double a = (14 * Math.Sin(x) + Math.Pow(y, 2)) / (0.92 * Math.Pow(Math.Cos(x), 3));
+Console.WriteLine(a);
+
