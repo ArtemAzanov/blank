a19208fcb44823d352617b4fc66f5b6a10e3eb3c
wgokef <verlevskaasofia@gmail.com>
2025-09-11 11:40:17 +0300

    js.secondWork

--- work2.js
+++ work2.js
@@ -1,9 +1,29 @@
 let x = +prompt("введите x")
-x = Math.sqrt(x)
-if(x < 1){
-    alert("Зелёный")
-}else if (x > 1  &&  x < 10){
-    alert("Жёлтый")
-}else if (x > 10){
-    alert("Красный")
+let y = +prompt("введите y")
+let a = +prompt("введите a")
+let b = +prompt("введите b")
+let z
+let maxEl
+let minEl
+if (x > 0  &&  y > 0){
+    maxEl = a/x
+    if (maxEl < b/x){
+        maxEl = b/x
+    }
+    if (maxEl < Math.sqrt(y)){
+        maxEl = Math.sqrt(y)
+        z = maxEl
+    }
 }
+else if (x < 0  &&  y > 0){
+    minEl = a * Math.pow(x , 3)
+    if (minEl > b * Math.pow(y , 2)){
+        minEl = b * Math.pow(y , 2)
+        z = minEl
+    }
+}
+else {
+    z = (Math.pow(2, x+y))
+}
+console.log(z)
+alert(z)
