882d0470236530a38058f8cf70d4e9bc101e7399
ArtemAzanov <azanov16@yandex.ru>
2025-09-11 10:11:39 +0300

    Add files via upload

---
+++ FirstWorkC++ 1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double x;
+	cout << "введите x: ";
+	cin >> x;
+	double y;
+	cout << "введите y: ";
+	cin >> y;
+    double a = (14 * sin(x) + pow(y, 2)) / (0.92 * pow(cos(x), 3));
+	cout << a << endl;
+	return 0;
+}
+
