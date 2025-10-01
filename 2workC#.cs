c28e6247b027768a14026ceb853017504c2a9a7a
ArtemAzanov <azanov16@yandex.ru>
2025-10-01 13:10:23 +0300

    2workC#

---
+++ 2workC#.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection.Emit;
+using System;
+
+Console.WriteLine("введите x:");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("введите y:");
+double y = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("введите a:");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("введите b:");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("введите z:");
+double z = Convert.ToDouble(Console.ReadLine());
+if (x > 0  &&  y > 0){
+    double maxEl = a / x; 
+    if (maxEl < b/x){
+        maxEl = b / x;
+    }
+    if (maxEl < Math.Sqrt(y))
+{
+        maxEl = Math.Sqrt(y);
+        z = maxEl;
+    }
+}
+else if (x < 0 && y > 0)
+{
+    double minEl = a * Math.Pow(x, 3);
+    if (minEl > b * Math.Pow(y, 2))
+    {
+        minEl = b * Math.Pow(y, 2);
+        z = minEl;
+    }
+}
+else
+{
+    z = (Math.Pow(2, x + y));
+    
+}
+Console.WriteLine(z);
