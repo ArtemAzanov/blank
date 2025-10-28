72b9638cc0f9e3a440bf21c396d0679efef39e83
ArtemAzanov <azanov16@yandex.ru>
2025-10-28 14:54:21 +0300

    Массив C#

---
+++ Programmassive.cs
@@ -0,0 +1,27 @@
+﻿
+    double[] array = { 5, -2, 0, 3, 4, 12, 7 };
+    double summa = 0;
+    double polozh = 0;
+    double size = 7;
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] > 0)
+        {
+            summa += array[i];
+            polozh++;
+        }
+    }
+    double W = summa / polozh;
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] > W)
+        {
+            array[i] = W;
+        }
+    }
+    for (int i = 0; i < size; i++)
+    {
+    Console.WriteLine(array[i]);
+    }
+Console.WriteLine("Среднее арифметическое число: " + W);
+
